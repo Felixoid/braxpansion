e342e318b95b0958c75d1f3670827435db118ae8
Mikhail f. Shiryaev <mr.felixoid@gmail.com>
2021-05-23 11:52:28 +0200

    Update doc for bytes

--- bytes/expand.go
+++ bytes/expand.go
@@ -10,7 +10,7 @@ var (
 	space = []byte(" ")
 )
 
-// Expand takes the string contains the shell expansion expression and returns list of strings after
+// Expand takes the []byte contains the shell expansion expression and returns a slice of []byte after
 // they are expanded. As in shells, each word is processed separately, so `12{1,2,3,4}as ds{1..3}22` produces `121as 122as 123as 124as ds122 ds222 ds322`
 func Expand(in []byte) ([][]byte, error) {
 	fields := bytes.Fields(in)
