507191c507d5fc388a140fecb08624982c76e8e0
Mikhail f. Shiryaev <mr.felixoid@gmail.com>
2021-05-24 08:45:30 +0200

    Kill mutants in strings/numbers.go

--- strings/numbers_test.go
+++ strings/numbers_test.go
@@ -16,11 +16,14 @@ func TestExpandNumbers(t *testing.T) {
 	tests := []data{
 		{getExpression("{-10..1}"), "-10 -9 -8 -7 -6 -5 -4 -3 -2 -1 0 1"},
 		{getExpression("{1..5}"), "1 2 3 4 5"},
+		{getExpression("{1..1}"), "1"},
 		{getExpression("{1..005..2}"), "001 003 005"},
+		{getExpression("{001..5..2}"), "001 003 005"},
 		{getExpression("{1..005..-002}"), "0005 0003 0001"},
 		{getExpression("{3..-02..3}"), "003 000"},
 		{getExpression("{3..-02..-3}"), "000 003"},
 		{getExpression("{3..-03..-3}"), "-03 000 003"},
+		{getExpression("{3..-03..0}"), "{3..-03..0}"},
 	}
 
 	for _, tt := range tests {
@@ -29,4 +32,9 @@ func TestExpandNumbers(t *testing.T) {
 		result := strings.Join(tt.in.expand(), space)
 		assert.Equal(t, tt.result, result, "input %q", tt.in)
 	}
+}
+
+func TestNumbersPanics(t *testing.T) {
+	tt := numbers{orig: []string{"-1", "-2", "1"}, seq: []int{-1, -2}}
+	assert.Panics(t, func() { tt.expand() })
 }
